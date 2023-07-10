4d5044cf5f82270cf2509bf86e1f8b59d6621367
Marlon <marlon.lalangui@epn.edu.ec>
2023-07-09 19:16:05 -0500

    registro paciente

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -19,11 +19,12 @@ const detallePaciente = async(req,res)=>{
 const registrarPaciente = async(req,res)=>{
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
     const nuevoPaciente = new Paciente(req.body)
-    nuevoPaciente.veterinario=req.body.id
+    console.log(req.veterinarioBDD._id)
+    nuevoPaciente.veterinario=req.veterinarioBDD._id
+    console.log(nuevoPaciente)
     await nuevoPaciente.save()
-    res.status(200).json({msg:"Registro exitoso del paciente"})
+    res.status(200).json(nuevoPaciente)
 }
-
 
 const actualizarPaciente = async(req,res)=>{
     const {id} = req.params
