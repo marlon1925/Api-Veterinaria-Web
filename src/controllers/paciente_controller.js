e9f02896c911709aa6d2421cdbfd48a4b29c98d1
Marlon <marlon.lalangui@epn.edu.ec>
2023-07-10 22:51:20 -0500

    swagger

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -33,6 +33,33 @@ import mongoose from "mongoose"
  *         nombre:
  *           type: string
  *           description: El nombre del paciente.
+ *         propietario:
+ *           type: string
+ *           description: El nombre del paciente.
+ *         email:
+ *           type: string
+ *           description: El correo del paciente
+ *         ceular: 
+ *           type: string
+ *           description: numero del paciente
+ *         convencional:
+ *           type: string
+ *           description: numero convencional del paciente
+ *         ingreso:
+ *           type: date
+ *           description: la fecha de ingreso
+ *         sintomas:
+ *           type: string
+ *           description: los sintomas del paciente
+ *         salida:
+ *           type: date
+ *           description: la fecha de salida
+ *         estado:
+ *           type: booleano
+ *           description: true o false
+ *         veterinario:
+ *           type: objeto
+ *           description: datos del veterinario 
  * 
  */ 
 const listarPacientes = async (req,res)=>{
@@ -40,6 +67,31 @@ const listarPacientes = async (req,res)=>{
     res.status(200).json(pacientes)
 }
 
+/**
+ * @swagger
+ * /ruta-de-la-api/detalle-paciente/{id}:
+ *   get:
+ *     summary: Detalle de paciente
+ *     description: Obtiene los detalles de un paciente por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del paciente
+ *     responses:
+ *       200:
+ *         description: Detalles del paciente obtenidos con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Paciente'
+ *       401:
+ *         description: No se proporcionó un token válido.
+ *       404:
+ *         description: No se encontró el paciente.
+ */
 
 const detallePaciente = async(req,res)=>{
     const {id} = req.params
@@ -48,6 +100,28 @@ const detallePaciente = async(req,res)=>{
     res.status(200).json(paciente)
 }
 
+/**
+ * @swagger
+ * /ruta-de-la-api/registrar-paciente:
+ *   post:
+ *     summary: Registrar paciente
+ *     description: Registra un nuevo paciente.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Paciente'
+ *     responses:
+ *       200:
+ *         description: Paciente registrado con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Paciente'
+ *       400:
+ *         description: Debes llenar todos los campos.
+ */
 
 const registrarPaciente = async(req,res)=>{
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
@@ -58,7 +132,41 @@ const registrarPaciente = async(req,res)=>{
     await nuevoPaciente.save()
     res.status(200).json(nuevoPaciente)
 }
-
+/**
+ * @swagger
+ * /ruta-de-la-api/actualizar-paciente/{id}:
+ *   put:
+ *     summary: Actualizar paciente
+ *     description: Actualiza los datos de un paciente existente por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del paciente
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Paciente'
+ *     responses:
+ *       200:
+ *         description: Actualización exitosa del paciente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   description: Mensaje de éxito
+ *       400:
+ *         description: Debes llenar todos los campos.
+ *       404:
+ *         description: No se encontró el paciente.
+ */
 const actualizarPaciente = async(req,res)=>{
     const {id} = req.params
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
@@ -67,7 +175,48 @@ const actualizarPaciente = async(req,res)=>{
     res.status(200).json({msg:"Actualización exitosa del paciente"})
 }
 
-
+/**
+ * @swagger
+ * /ruta-de-la-api/eliminar-paciente/{id}:
+ *   put:
+ *     summary: Eliminar paciente
+ *     description: Actualiza el estado y la fecha de salida de un paciente existente por su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID del paciente
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               salida:
+ *                 type: string
+ *                 format: date
+ *                 description: Fecha de salida del paciente (en formato ISO 8601)
+ *             required:
+ *               - salida
+ *     responses:
+ *       200:
+ *         description: Fecha de salida del paciente registrada exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   description: Mensaje de éxito
+ *       400:
+ *         description: Debes llenar todos los campos.
+ *       404:
+ *         description: No se encontró el paciente.
+ */
 
 const eliminarPaciente = async (req,res)=>{
     const {id} = req.params
--- src/routers/paciente_routes.js
+++ src/routers/paciente_routes.js
@@ -10,42 +10,7 @@ import {
     registrarPaciente,
 } from "../controllers/paciente_controller.js";
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     verificarAutenticacionMiddleware:
- *       type: object
- *       properties:
- *         msg:
- *           type: string
- *           description: Mensaje de error si no se proporciona un token.
- */
 
-/**
- * @swagger
- * /ruta-a-la-que-se-aplica-el-middleware:
- *   get:
- *     summary: Descripción breve de la ruta
- *     description: Descripción detallada de la ruta y su funcionalidad.
- *     security:
- *       - BearerAuth: []
- *     responses:
- *       200:
- *         description: Respuesta exitosa.
- *       401:
- *         description: No se proporcionó un token válido.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/verificarAutenticacionMiddleware'
- *       404:
- *         description: No se encontró el recurso.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/verificarAutenticacionMiddleware'
- */
 router.get("/pacientes", verificarAutenticacion, listarPacientes);
 router.get("/paciente/:id", verificarAutenticacion, detallePaciente);
 router.post("/paciente/registro", verificarAutenticacion, registrarPaciente);
